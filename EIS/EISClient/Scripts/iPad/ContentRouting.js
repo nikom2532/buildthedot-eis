570f41fcbd595982e3e0e968a3b4cf9596328abf
buildthedot3 <dev3@buildthedot.com>
2013-07-12 13:48:03 +0700

    duplicate the Charts files for the iPad size

--- EIS/EISClient/Scripts/iPad/ContentRouting.js
+++ EIS/EISClient/Scripts/iPad/ContentRouting.js
@@ -1,16 +1,16 @@
 ﻿var ContentRouting = {
     numberOfEmployee: {
         title: "อัตรากำลัง",
-        imageUrl: "Charts/NumberOfEmployee.ashx",
+        imageUrl: "Charts/iPad/NumberOfEmployee.ashx",
         draggable: false,
         buttons: {
             detail: {
                 title: "อัตรากำลัง - รายละเอียด",
-                imageUrl: "Charts/NumberOfEmployeeDetail.ashx"
+                imageUrl: "Charts/iPad/NumberOfEmployeeDetail.ashx"
             },
             compare: {
                 title: "อัตรากำลัง - เปรียบเทียบรายปี",
-                imageUrl: "Charts/NumberOfEmployeeCompare.ashx",
+                imageUrl: "Charts/iPad/NumberOfEmployeeCompare.ashx",
                 draggable: false,
                 hideYear: true
             }
@@ -18,43 +18,43 @@
     },
     estimatedRetirement: {
         title: "ประมาณการอัตรากำลังเกษียณ",
-        imageUrl: "Charts/EstimatedRetirement.ashx",
+        imageUrl: "Charts/iPad/EstimatedRetirement.ashx",
         buttons: {},
         draggable: false,
         yearOffset: 1
     },
     estimatedFundMember: {
         title: "สมาชิก กบข./กสจ.",
-        imageUrl: "Charts/EstimatedFundMember.ashx",
+        imageUrl: "Charts/iPad/EstimatedFundMember.ashx",
         draggable: false,
         buttons: {
             compareonly: {
                 title: "สมาชิก กบข./กสจ. - เทียบรายปี",
-                imageUrl: "Charts/EstimatedFundmemberCompare.ashx",
+                imageUrl: "Charts/iPad/EstimatedFundmemberCompare.ashx",
                 draggable: false
             }
         }
     },
     budgetUsage: {
         title: "การเบิกจ่ายเงินงบประมาณภาพรวม",
-        imageUrl: "Charts/BudgetUsage.ashx",
+        imageUrl: "Charts/iPad/BudgetUsage.ashx",
         draggable: false,
         buttons: {
             compareonly: {
                 title: "การเบิกจ่ายเงินงบประมาณภาพรวม - เทียบรายปี",
-                imageUrl: "Charts/BudgetUsageCompare.ashx",
+                imageUrl: "Charts/iPad/BudgetUsageCompare.ashx",
                 draggable: false
             }
         }
     },
     employeeRelatedBudgetUsage: {
         title: "การจ่ายเงินงบบุคลากร",
-        imageUrl: "Charts/EmployeeRelatedBudget.ashx",
+        imageUrl: "Charts/iPad/EmployeeRelatedBudget.ashx",
         draggable: false,
         buttons: {
             compareonly: {
                 title: "การจ่ายงบบุคลากร - เปรียบเทียบรายปี",
-                imageUrl: "Charts/EmployeeRelatedBudgetCompare.ashx",
+                imageUrl: "Charts/iPad/EmployeeRelatedBudgetCompare.ashx",
                 draggable: true,
                 height: 2
             }
@@ -62,39 +62,39 @@
     },
     retirementFundUsage: {
         title: "การเบิกจ่ายบำเหน็จบำนาญ",
-        imageUrl: "Charts/RetirementFundUsage.ashx",
+        imageUrl: "Charts/iPad/RetirementFundUsage.ashx",
         draggable: false,
         buttons: {}
     },
     warrantFundProviding: {
         title: "การอนุมัติเงินกู้ของสถาบันการเงิน (จำนวนเงิน)",
-        imageUrl: "Charts/BankLoanApproval.ashx",
+        imageUrl: "Charts/iPad/BankLoanApproval.ashx",
         draggable: false,
         buttons: {
             one: {
                 title: "การอนุมัติเงินกู้ของสถาบันการเงิน",
-                imageUrl: "Charts/BankLoanApproval.ashx",
+                imageUrl: "Charts/iPad/BankLoanApproval.ashx",
                 draggable: false,
                 buttonText: "อนุมัติกู้<br>(เงิน)",
                 fontSize: 8
             },
             two: {
                 title: "การอนุมัติเงินกู้ของสถาบันการเงิน",
-                imageUrl: "Charts/BankLoanApprovalToPeople.ashx",
+                imageUrl: "Charts/iPad/BankLoanApprovalToPeople.ashx",
                 draggable: false,
                 buttonText: "อนุมัติกู้<br>(คน)",
                 fontSize: 8
             },
             three: {
                 title: "การออกหนังสือรับรองสิทธิ์",
-                imageUrl: "Charts/WarrantFundProviding.ashx",
+                imageUrl: "Charts/iPad/WarrantFundProviding.ashx",
                 draggable: false,
                 buttonText: "หนังสือรับรอง<br>(เงิน)",
                 fontSize: 8
             },
             four: {
                 title: "การออกหนังสือรับรองสิทธิ์",
-                imageUrl: "Charts/WarrantFundProvidingToPeople.ashx",
+                imageUrl: "Charts/iPad/WarrantFundProvidingToPeople.ashx",
                 draggable: false,
                 buttonText: "หนังสือรับรอง<br>(คน)",
                 fontSize: 8
@@ -103,19 +103,19 @@
     },
     firstCarStatus: {
         title: "รถคันแรก - จำนวนคน",
-        imageUrl: "Charts/FirstCarStatusByPeople.ashx",
+        imageUrl: "Charts/iPad/FirstCarStatusByPeople.ashx",
         draggable: false,
         buttons: {
             one: {
                 title: "รถคันแรก - จำนวนคน",
-                imageUrl: "Charts/FirstCarStatusByPeople.ashx",
+                imageUrl: "Charts/iPad/FirstCarStatusByPeople.ashx",
                 draggable: false,
                 buttonText: "จำนวนคน",
                 fontSize: 10
             },
             two: {
                 title: "รถคันแรก - จำนวนเงิน",
-                imageUrl: "Charts/FirstCarStatusByAmount.ashx",
+                imageUrl: "Charts/iPad/FirstCarStatusByAmount.ashx",
                 draggable: false,
                 buttonText: "จำนวนเงิน",
                 fontSize: 10
